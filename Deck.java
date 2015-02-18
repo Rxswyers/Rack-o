af668c318183b13544d64f3341530046b86829f3
Rxswyers <rxswyers@live.com>
2015-02-18 12:48:12 -0500

    Added Player class, and edited Deck

--- Deck.java
+++ Deck.java
@@ -1,6 +1,16 @@
 public class Deck
 {
+	//members
 	Stack<Card> Pile = new Stack<Card>();
-	
 
+	//methods
+	public void addCard(Card C)
+	{
+		this.Pile.push(C);
+	}
+	public Card draw()
+	{
+		return this.Pile.pop();
+	}
+	
 }
---
+++ Player.java
@@ -0,0 +1,5 @@
+public class Player
+{
+  Rack Hand = new Rack();
+  
+}
