b768f8ec96c10461bebd5c4e5456ea9c5a7e5f0f
Rxswyers <rxswyers@live.com>
2015-02-21 10:56:27 -0500

    Reverted Racko.java and made a new test file

--- Racko.java
+++ Racko.java
@@ -1,70 +1,53 @@
+
 import java.util.*;
 public class Racko
 {
-	public static void main(String args[])
+	//members
+	ArrayList<Player> Players = new ArrayList<Player>();
+	ArrayList<Card> Cards = new ArrayList<Card>();
+	Deck DrawPile = new Deck();
+	Deck DiscardPile = new Deck();
+	//methods
+	public void Racko()
 	{
-		ArrayList<Player> Players = new ArrayList<Player>();
-		ArrayList<Card> Cards = new ArrayList<Card>();
-		Deck DrawPile = new Deck();
-		Deck DiscardPile = new Deck();
-		int cardSize = 0;
-
-		Player P1 = new Player();
-		Player P2 = new Player();
-		Players.add(P1);
-		Players.add(P2);
-
-		//Sets the number of cards based on how many players there are
-		//2 Players - 40 Cards
-		//3 Players - 50 Cards
-		//4 Players - 60 Cards
+		int cardSize;
 		switch(Players.size())
 		{
 			case 2:
-				cardSize = 40;
-				break;
+			cardSize = 40;
+			break;
 			case 3:
-				cardSize = 50;
-				break;
+			cardSize = 50;
+			break;
 			case 4:
-				cardSize = 60;
-				break;
+			cardSize = 60;
+			break;
 		}
-		//Creates the cards
 		for(int i = 1; i <= cardSize; i ++)
 		{
-			Cards.add(new Card(i));
+			this.Cards.add(new Card(this));
 		}
-		//shuffles the cards up to be put in the DrawPile(Deck)
 		Collections.shuffle(Cards);
-
-
-
-		deal(Players,Cards,DrawPile);
-
-		System.out.println(DrawPile);
 	}
-	public void deal(ArrayList<Player> Ps, ArrayList<Cards> Cs, Deck D)
+	public void addPlayer(Player P)
+	{
+		return this.Players.add(P);
+	}
+	public void deal()
 	{
 		int count = 0;
-		for(int i = 0; i < Ps.size(); i++)
+		for(int i = 0; i < Players.size(); i++)
 		{
-			Ps.get(i).pickupCard(Cs.get(count));
+			Players.get(i).pickupCard(Cards.get(count));
 			count++;
 		}
-		for(int j = count; j < Ps.size(); j++)
-		{
-			D.push(Cs.get(j));
-		}
 	}
-	/*
-	public void populateDeck()
-	{
-
-	}
-	public void swapDecks()
-	{
-
-	}
-	*/
+/*
+public void populateDeck()
+{
+}
+public void swapDecks()
+{
+}
+*/
 }
