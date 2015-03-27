e79e2973dc363a05963e5cd0b858022a035e89e0
Rxswyers <rswyers@boutit.tk>
2015-03-27 08:42:29 -0400

    Dealing with placements

--- GUI/ImageTest.java
+++ GUI/ImageTest.java
@@ -1,5 +1,5 @@
 /*Author: 			Ruben Swyers
-* Creation Date: 	March 15, 2015 
+* Creation Date: 	March 15, 2015
 * Due Date: 		March 28, 2015
 * Course: 			CSC243
 * Professor Name: 	Dr. Spiegel
@@ -21,8 +21,8 @@ public class ImageTest extends JApplet implements ActionListener
   {
 	setLayout(null);
 	setSize(800,600);
-	
-    String Images[] = {"blacksailscard.jpg","blacksailsback.jpg"};
+
+    String Images[] = {"newblacksailscard.jpg","blacksailsback.jpg"};
     image[0] = getImage(getCodeBase(), Images[0]);
     imageIcons[0] = new ImageIcon(image[0]);
 
@@ -39,39 +39,40 @@ public class ImageTest extends JApplet implements ActionListener
     }
     catch (InterruptedException e)
     {}
-	
+
     showStatus("Loaded Image");
 	Deck D = new Deck();
-	D.setBounds(10,10,300,300);
+	D.setBounds(10,10,250,250);
 	this.add(D);
 	D.setLayout(null);
-	
-	
+
+
 	//Card Cs[] = new Card[2];
-	
+
 	Card CTest = new Card(Images[0],imageIcons[0],30);
 	CTest.addActionListener(this);
-	
+
 	Card CTest2 = new Card(Images[0],imageIcons[0],29);
 	CTest2.addActionListener(this);
 	D.addCard(CTest,1,20,20);
 	D.addCard(CTest2,2,22,22);
-	
+
 	Rack R = new Rack();
-	R.setBounds(0,300,500,300);
+	R.setBounds(0,400,800,200);
 	this.add(R);
 	R.setLayout(null);
-	
+
 	Card C;
-	int xOffset = 40;
-	int yOffset = -15;
+	int xOffset = 25;
+	int yOffset = -12;
 	int last = 10;
 	for (int i = 1; i < 11; i ++)
 	{
 		C = new Card(Images[0],imageIcons[0],i);
 		C.addActionListener(this);
 		C.setActionCommand(Integer.toString(i));
-		C.setBounds(120+(xOffset * (i-1)),120+(yOffset*(i-1)),95,140);
+		//C.setBounds(120+(xOffset * (i-1)),100+(yOffset*(i-1)),95,140);
+    C.setBounds(120+(xOffset * (i-1)),130+(yOffset*(i-1)),110,60);
 		R.addCard(C,new Integer(10 - i));
 	}
 	this.setVisible(true);
