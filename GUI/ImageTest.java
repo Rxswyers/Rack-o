8d7f7d21e889309737ad721b729ff1e94e9996e1
Ruben Swyers <rxswyers@live.com>
2015-03-26 21:17:03 -0400

    Updated to Null layout

--- GUI/ImageTest.java
+++ GUI/ImageTest.java
@@ -19,7 +19,7 @@ public class ImageTest extends JApplet implements ActionListener
 
   public void init()
   {
-	setLayout(new CardLayout(0,0));
+	setLayout(null);
 	setSize(800,600);
 	
     String Images[] = {"blacksailscard.jpg","blacksailsback.jpg"};
@@ -42,20 +42,26 @@ public class ImageTest extends JApplet implements ActionListener
 	
     showStatus("Loaded Image");
 	Deck D = new Deck();
-	add(D);
-	Card C[] = new Card[2];
-	D.setLayout(new CardLayout(0,0));
+	D.setBounds(10,10,300,300);
+	this.add(D);
+	D.setLayout(null);
+	
+	
+	//Card Cs[] = new Card[2];
+	
 	Card CTest = new Card(Images[0],imageIcons[0],30);
 	CTest.addActionListener(this);
 	
 	Card CTest2 = new Card(Images[0],imageIcons[0],29);
 	CTest2.addActionListener(this);
-	D.addCard(CTest,1,10,10);
-	D.addCard(CTest2,2,12,12);
+	D.addCard(CTest,1,20,20);
+	D.addCard(CTest2,2,22,22);
 	
 	Rack R = new Rack();
-	add(R);
-	R.setLayout(new CardLayout(0,0));
+	R.setBounds(0,300,500,300);
+	this.add(R);
+	R.setLayout(null);
+	
 	Card C;
 	int xOffset = 40;
 	int yOffset = -15;
@@ -65,10 +71,11 @@ public class ImageTest extends JApplet implements ActionListener
 		C = new Card(Images[0],imageIcons[0],i);
 		C.addActionListener(this);
 		C.setActionCommand(Integer.toString(i));
-		C.setBounds(120+(xOffset * (i-1)),450+(yOffset*(i-1)),95,140);
+		C.setBounds(120+(xOffset * (i-1)),120+(yOffset*(i-1)),95,140);
 		R.addCard(C,new Integer(10 - i));
 	}
-
+	this.setVisible(true);
+	this.validate();
   }
   public void actionPerformed(ActionEvent e)
   {
