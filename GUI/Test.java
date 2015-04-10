b3807166692955eac631b6c7ea1d2baa30870de3
Ruben <rxswyers@live.com>
2015-04-10 11:46:53 +0000

    Started testing again, fixed bug in cheat in jar

--- GUI/Test.java
+++ GUI/Test.java
@@ -1,24 +1,41 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-
+import java.util.ArrayList;
 public class Test extends JFrame implements WindowListener, ActionListener
 {
 	//members
 	Button B;
-
+	ArrayList<Button> Bs = new ArrayList<Button>();
 	public Test(String s)
 	{
 		super(s);
 		JPanel panel = new JPanel(new BorderLayout(5,5));
 
-		setSize(250,200);
-		panel.setSize(250,250);
+		setSize(700,600);
+		panel.setBounds(0,0,800,600);
 		addWindowListener(this);
-		B = new Button("Testing");
-		panel.add("East",B);
-		B.addActionListener(this);
-		getContentPane().add(panel);
+		char letter = 'A';
+		int xOff = 50;
+		int yOff = 50;
+		for(int i = 0; i < 2; i ++)
+		{
+			for(int j = 0; j < 13; j ++)
+			{
+				B = new Button(Character.toString(letter));
+				B.setBounds(25 + (xOff * j),415 + (yOff *i),45,40);
+				this.add(B);
+				Bs.add(B);
+				B.addActionListener(this);
+				letter ++;
+			}
+		}
+		//for(Button button:Bs)
+		//{
+			//button.setEnabled(false);
+		//}
+
+		add(panel);
 		setVisible(true);
 	}
 	public void windowClosed(WindowEvent event) {}
@@ -31,7 +48,8 @@ public class Test extends JFrame implements WindowListener, ActionListener
 
 	public void actionPerformed(ActionEvent event)
 	{
-		System.out.println("poop");
+		System.out.println("Worked "  + ((Button)event.getSource()).getActionCommand());
+		((Button)event.getSource()).setEnabled(false);
 	}
 	public static void main(String []args)
 	{
